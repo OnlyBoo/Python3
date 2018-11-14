cf7d9f9b0b3a57029c0d7eed7e00a8d725082326
OnlyBoo <j.burgess@lancaster.ac.uk>
2018-11-14 13:29:41 +0000

    changed files

--- second.py
+++ second.py
@@ -1,3 +1,4 @@
+"""
 mylist = []
 mylist.append(1)
 mylist.append(2)
@@ -7,3 +8,21 @@ print(mylist[0:2])
 
 for rec in mylist:
     print rec
+
+"""
+
+
+numbers = []
+strings = []
+names = ["John", "Eric", "Melissa"]
+
+second_name = names[1]
+
+numbers.append([0,1,2,3,4,5,6,7,8,9,10])
+strings.append(["Hello","World"])
+
+
+
+print(numbers)
+print(strings)
+print("The second names on the names list is %s" %second_name)
